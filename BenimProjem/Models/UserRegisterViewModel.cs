a7064d773724a630423eddd013c994f6206b6203
keremkul0 <keremkul0@outlook.com>
2023-09-27 17:03:42 +0300

    signup sayfası açılmıyor

---
+++ BenimProjem/Models/UserRegisterViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BenimProjem.Models
+{
+    public class UserRegisterViewModel
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string UserName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter your email")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter your birthdate")]
+        public DateOnly Birthdate { get; set; }
+
+        public string Gender { get; set; } = "Do not want to specify";
+        
+        [Required(ErrorMessage = "Please Enter Your password")]
+        public string Password = string.Empty;
+
+        [Required(ErrorMessage = "Please Enter Your Password Again")]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
